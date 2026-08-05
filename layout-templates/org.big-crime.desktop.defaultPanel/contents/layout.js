157a5718545e3d2cfeefb94640bfa7cef5e6c810
Torsten Wohlfarth <towo@siduction.org>
2026-08-05 19:38:57 +0200

    fix previews for light theme

--- layout-templates/org.big-crime.desktop.defaultPanel/contents/layout.js
+++ layout-templates/org.big-crime.desktop.defaultPanel/contents/layout.js
@@ -6,7 +6,7 @@ var panelScreen = panel.screen
 // For an Icons-Only Task Manager on the bottom, *3 is too much, *2 is too little
 // Round down to next highest even number since the Panel size widget only displays
 // even numbers
-panel.height = 2 * Math.floor(gridUnit * 2.5 / 2)
+panel.height = 2 * Math.floor(gridUnit * 2.2 / 2)
 
 // Restrict horizontal panel to a maximum size of a 21:9 monitor
 const maximumAspectRatio = 21/9;
